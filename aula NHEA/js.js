5fae15db59040cd4a7dc195c8e6994840a6eb14c
Kepwhite <80289790+Kepwhite@users.noreply.github.com>
2021-05-03 21:35:50 -0400

    Add files via upload

---
+++ aula NHEA/js.js
@@ -0,0 +1,19 @@
+$(document).ready(
+    function () {
+        $.ajax({
+            type: 'GET',
+            url: "http://192.168.10.85:9000/users",
+            success: function(data){
+                $.each(data, function(i, user){
+                    console.log(user.name)
+                    $("#NomeDaTable").append(
+                    "<tr><td>"+user.name+"<td/><tr/>"
+                )
+            }
+                )},
+            error: function(data){
+                console.log(data);
+            }
+        })
+    }
+)
